1348b4f826765fb6a146b972aeca8b291fc302d4
MariyaZima <100530301+MariyaZima@users.noreply.github.com>
2024-02-29 21:06:34 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN = '6726997767:AAFFupa1NL5QOU94Q788ftogRpgnK1mMnZs'
+API_key = '3dab4bdb6a337a429d6a1b96b23a57ccd66ddb511bdaa601623d98b7bf11e483'
+keys = {
+    'рубль': 'RUB',
+    'евро': 'EUR',
+    'доллар': 'USD',
+}
